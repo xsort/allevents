f62a419b94851c4242c7e96ed0509f002d9bc619
alex <alexzc@mail.ru>
2016-05-31 17:16:34 +0300

    Скрыл верхний баннер и поставил деньги в кошельке на 0

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -46,7 +46,7 @@
     
 
     <div class="container wrapper">
-        @include('partials.topbar')
+        <!-- @include('partials.topbar') -->
         @include('partials.header')
         <div class="layout-page ">
             <div class="layout-content" >
