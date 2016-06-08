66ae082cae2212d488ebccff47bc87db119e7493
pokabudki <alexandr285@gmail.com>
2016-06-08 17:16:32 +0300

    Сделали выбор родителя для категории

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -7,6 +7,7 @@
         <h1> <a href="{{ URL::to('admin/categories') }}">Категории</a> <small><i class="ace-icon fa fa-angle-double-right"></i> Редактирование категории </small> </h1>
     </div>
 
+
     @include('admin.partials.errors')
 
     @if(!isset($data))
@@ -24,11 +25,33 @@
                 </div>
             </div>
             <div class="form-group">
+                {{ Form::label('name', 'Заголовок рум', ['class'=>'col-sm-3 control-label no-padding-right']) }}
+                <div class="col-sm-9">
+                    {{ Form::text('name[ro]', (isset($data->name_ro) ? $data->name_ro : old('name_ro')), array('class' => 'col-sm-11 col-xs-12')) }}
+                </div>
+            </div>
+            <div class="form-group">
+                {{ Form::label('name', 'Заголовок англ', ['class'=>'col-sm-3 control-label no-padding-right']) }}
+                <div class="col-sm-9">
+                    {{ Form::text('name[en]', (isset($data->name_en) ? $data->name_en : old('name_en')), array('class' => 'col-sm-11 col-xs-12')) }}
+                </div>
+            </div>
+            <div class="form-group">
                 {{ Form::label('slug', 'URL', ['class'=>'col-sm-3 control-label no-padding-right']) }}
                 <div class="col-sm-9">
                     {{ Form::text('slug', (isset($data->slug) ? $data->slug : old('slug')), array('class' => 'col-sm-11 col-xs-12')) }}
                 </div>
             </div>
+            
+            <div class="form-group">
+				{{ Form::label('parent', 'родитель', ['class'=>'col-sm-3 control-label no-padding-right']) }}
+				
+				<div class="col-sm-5">
+					{{ Form::select('parent[]', $categories, $parents, ['multiple'=>'multiple','id'=>'chosencat','class'=>'col-sm-11 control-label no-padding-right']) }}
+				</div>
+			</div>
+				
+			</div>
             
         </div><!-- /.col-sm-6 -->
 
@@ -69,7 +92,12 @@
         <div class="tab-content">
             <div class="tab-pane active" id="ru">
                 {{ Form::textarea('description[ru]', (isset($data->description) ? $data->description : old('description')), array('class' => 'ckeditor', 'id' => 'editor')) }}
+                {{ Form::textarea('description[ro]', (isset($data->description_ro) ? $data->description_ro : old('description_ro')), array('class' => 'ckeditor', 'id' => 'editor')) }}
+                {{ Form::textarea('description[en]', (isset($data->description_en) ? $data->description_en : old('description_en')), array('class' => 'ckeditor', 'id' => 'editor')) }}
              </div>
+             
+              
+             
              @include('admin.partials.meta')
             @include('admin.partials.photos', ['table' => 'categories', 'table_id' => isset($data->id) ? $data->id : 0])
 </div>
