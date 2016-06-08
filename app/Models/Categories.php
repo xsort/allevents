a5c332425904cf9658185fb649416245b9c5eabb
pokabudki <alexandr285@gmail.com>
2016-06-08 17:16:37 +0300

    Сделали выбор родителя для категории

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -10,4 +10,15 @@ class Categories extends BaseModel
     public function children() {
 	    return $this->belongsToMany('App\Models\Categories', 'categories_xref', 'parent_id', 'child_id');
     }
+    
+    public function parents() {
+	    return $this->belongsToMany('App\Models\Categories', 'categories_xref',  'child_id', 'parent_id');
+    }
+    
+/*
+    public function getParentCategories() {
+	    $temp = $this->belongsToMany('App\Models\Categories', 'categories_xref', 'parent_id', 'child_id');
+	    
+    }
+*/
 }
