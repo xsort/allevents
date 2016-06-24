f352d121ab17d1b0ae9e898d730ca194d1b9f6e3
alex <alexzc@mail.ru>
2016-06-24 20:57:29 +0300

    Поправил вывод фоток новостей в сайдбаре. Truncate по кол-во символов у новостей в сайдбаре

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -167,4 +167,37 @@
     <script>
         $("#chosencat").chosen();
     </script>
+     <script>
+
+        $(document).ready(function(){
+         initSEFonEnter();   
+        });
+
+        transliterate = (
+        function() {
+            var
+                rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
+                eng = "shh sh ch cz yu ya yo zh i  y  e  a b v g d e z i j k l m n o p r s t u f x i".split(/ +/g)
+            ;
+            return function(text, engToRus) {
+                var x;
+                for(x = 0; x < rus.length; x++) {
+                    text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
+                    text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());
+                }
+                text = text.toLowerCase().replace(/[^a-zA-Z ]/g, "").split(' ').join('-');
+                return text;
+            }
+        }
+    )();
+
+    function initSEFonEnter(){
+        $("input.name_ru").keyup(function() {
+            var a = $(this).val();
+            var b = transliterate(a);
+            $("input[name=slug]").val(b);
+        });
+    }
+
+    </script>
 @endsection
