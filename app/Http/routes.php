2b4c85812a325345ff60d82ed3c03ede70081ceb
pokabudki <alexandr285@gmail.com>
2016-06-29 15:56:47 +0300

    Удаление фоток у товаров.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -105,6 +105,7 @@ Route::group(['middleware' => ['web', 'admin'], 'prefix' => 'admin', 'as' => 'ad
     Route::any('photos/upload', 'Admin\PhotosController@upload');
     Route::get('photos/getphotos', 'Admin\PhotosController@getJSONPhotos');
     Route::get('photos/changesort', 'Admin\PhotosController@changesort');
+    Route::get('photos/delete/{id}', 'Admin\PhotosController@destroy');
 
 //URL get content
 //Route::get('{slug}', ['as' => 'getURL', 'uses' => 'ContentController@getBySlug', 'middleware'  => ['web']]);
